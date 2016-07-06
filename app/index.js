d88e84d2c6580822e43506fea8883c045c39de16
Alejandro Nanez <alejonanez@gmail.com>
2016-07-05 22:13:23 -0500

    Calling Wikipedia API and displaying data

--- app/index.js
+++ app/index.js
@@ -21,7 +21,36 @@ const URL = 'http://en.wikipedia.org/w/api.php?' +
 const requestStream = Observable.just(URL);
 const responseStream = requestStream
 						.flatMap(() => Observable.fromPromise(fetchJsonp(URL)))
-						.flatMap(response => response.json());
+						.flatMap(response => response.json())
+						.map(response => response.query.pages);
 
-responseStream.subscribe(({ query: { pages } }) => console.log(pages));
+
+function createArticle(searchResult) {
+	const {
+		title,
+		extract
+	} = searchResult;
+
+	const article = `
+		<header>
+			<h2>${title}</h2>
+			<p>${extract}</p>
+			<a href="http://en.wikipedia.org/wiki/${title.replace(' ', '_')}" target="_blank">See it on wikipedia</a>
+		</header>
+	`;
+
+	const element = document.createElement('article');
+	element.classList = 'pure-u-5-5 article';
+	element.innerHTML = article;
+
+	return element;
+}
+
+responseStream.subscribe((articles) => {
+	const container = document.querySelector('.article-container');
+
+	Object.keys(articles).forEach((article) => {
+		container.appendChild(createArticle(articles[article]));
+	});
+});
 /* eslint-enable no-console */
